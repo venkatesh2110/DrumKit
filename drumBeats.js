cd38d26f2d8f115f2a44a7fbee7eef613674e631
Venkatesh Prasath <54813384+venkatesh2110@users.noreply.github.com>
2020-07-01 17:15:23 +0530

    Initial

---
+++ drumBeats.js
@@ -0,0 +1,50 @@
+function toggleFunction() {
+   document.getElementById('drumsPage').style.display = 'block';
+   document.getElementById('homePage').style.display = 'none';
+}
+
+let selectDrumClassLength = document.getElementsByClassName('selectDrum').length;
+for (let i = 0; i < selectDrumClassLength; i++) {
+   document.getElementsByClassName("selectDrum")[i].addEventListener("click", function(){
+      var innerHTMLvalue = this.innerHTML;
+      alertFunction(innerHTMLvalue);
+   });
+
+   document.getElementsByClassName("selectDrum")[i].addEventListener("keypress",function(event){
+      var innerTextvalue = event.key;
+      alertFunction(innerTextvalue);
+   })
+}
+function alertFunction(text) {
+   
+   switch (text) {
+      case 'w':
+         let bass = new Audio('sounds/bass.wav');
+         bass.play();
+         break;
+
+      case 'a':
+         let snare = new Audio('sounds/snare.wav');
+         snare.play();
+         break;
+
+      case 'd':
+         let toms = new Audio('sounds/toms.wav');
+         toms.play();
+         break;
+
+      case 'j':
+         let cymbals = new Audio('sounds/cymbals.wav');
+         cymbals.play();
+         break;
+
+      case 'l':
+         let splashcymbals = new Audio('sounds/splashcymbals.mp3');
+         splashcymbals.play();
+         break;
+
+      default:
+         console.log('Error');
+         break;
+   }
+}
